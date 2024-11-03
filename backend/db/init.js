a29ec80e148db2a32e3488159221db0bd1e49058
Nikita Donskoy <donskoy.nikita13@gmail.com>
2024-11-04 01:58:01 +0200

    deploy fix

--- backend/db/init.js
+++ backend/db/init.js
@@ -1,9 +1,9 @@
 const admin = require("firebase-admin");
-const serviceAccount = require(`../../${process.env.FIREBASE_CREDENTIALS}`);
+//const serviceAccount = require(`../../${process.env.FIREBASE_CREDENTIALS}`);
 
 function init(){
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CREDENTIALS)),
     databaseURL: process.env.DB_URL
   });
 }
